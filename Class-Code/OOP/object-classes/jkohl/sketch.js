20086967823061cacafffe38be0c40dfc7cb9e7d
Jacob Kohl <jkohlpenn@Jacobs-MacBook-Pro-3.local>
2019-04-12 09:03:23 -0400

    new bubbles

--- Class-Code/OOP/object-classes/jkohl/sketch.js
+++ Class-Code/OOP/object-classes/jkohl/sketch.js
@@ -20,25 +20,28 @@ class Bubble {
         this.y += random(-height / 100, height / 100);
     }
     check(){
-        if(x + r >= width || x - r <= 0) xVel *= -1;
-        if(y + r >= height || y - r <= 0) yVel *= -1;
+        if(this.x + this.r >= width || this.x - this.r <= 0) this.xVel *= -1;
+        if(this.y + this.r >= height || this.y - this.r <= 0) this.yVel *= -1;
     }
 }
 
-let bubbles = new Array(2);
+let bubbles = [];
 
 function setup() {
     createCanvas(600, 400);
     noStroke();
 
     ellipseMode(RADIUS)
+
+    bubbles.push(new Bubble(250, 50, 60))
+    bubbles.push(new Bubble(30, 40, 50))
 }
 
 function draw() {
     background(0,50);
 
     for(let i = 0; i < bubbles.length; ++i){
-        bubbles[i].appear();
+        bubbles[i].display();
         bubbles[i].move();
         bubbles[i].check();
     }
